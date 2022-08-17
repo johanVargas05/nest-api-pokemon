e8514e492561b163d5f5add6e2c0a01b16cae898
Henry Johan Vargas <sactrom@gmail.com>
2022-08-17 13:27:37 -0500

    project init

---
+++ README.md
@@ -0,0 +1,22 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+# Ejecutar en desarrollo
+1. Clonar el repositorio
+2. ejecutar
+```
+yarn install
+```
+3. Tener NEST JS CLI instalado en dado caso de no tenerlo ejecutar el siguiente comando: 
+```
+npm i -g @nestjs/cli
+``` 
+4. Levantar la base de datos 
+```
+docker-compose up -d
+```
+
+## Stack usado
+* MongoDB
+* Nest js
---
+++ src/pokemon/pokemon.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { PokemonService } from './pokemon.service';
+import { PokemonController } from './pokemon.controller';
+import { Pokemon, pokemonSchema } from './entities/pokemon.entity';
+
+@Module({
+  controllers: [PokemonController],
+  providers: [PokemonService],
+  imports: [MongooseModule.forFeature([{
+    name: Pokemon.name,
+    schema:pokemonSchema
+  }])]
+})
+export class PokemonModule {}
